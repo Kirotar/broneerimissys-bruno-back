8ce999fb8066873dfbbd234c11108ea98c867de7
lea.kirotar <lea.pullerits@gmail.com>
2025-03-06 13:20:41 +0200

    Made RoomSearch DTO

---
+++ src/main/java/ee/rara/bruno/bruno/dto/RoomSearch.java
@@ -0,0 +1,36 @@
+package ee.rara.bruno.bruno.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+public class RoomSearch {
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime startDateTime;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime endDateTime;
+
+    private Integer minCapacity;
+
+    private Integer floor;
+
+    //Currently room table rooms don't have good keywords
+    private List<String> keywords;
+
+
+    // Currently not in use, ideas for future
+   /* private BigDecimal maxPrice;
+    private List<String> requiredEquipment;
+    private String roomType;
+    private Boolean hasVideoConference;
+    private Boolean hasWhiteboard;
+    private Boolean isAccessible;*/
+
+}
