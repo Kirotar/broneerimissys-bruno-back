e227d0e72dc3ee8b9abc81454b259cf0f53385fc
lea.kirotar <lea.kirotar@rara.ee>
2025-03-13 16:42:17 +0200

    Added query to get bookings by userId.

--- src/main/java/ee/rara/bruno/bruno/repository/BookingRepository.java
+++ src/main/java/ee/rara/bruno/bruno/repository/BookingRepository.java
@@ -2,9 +2,11 @@ package ee.rara.bruno.bruno.repository;
 
 import ee.rara.bruno.bruno.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
+    @Query("SELECT b FROM Booking b WHERE b.user = :userId")
     List<Booking> findAllByUserId(int userId);
 }
