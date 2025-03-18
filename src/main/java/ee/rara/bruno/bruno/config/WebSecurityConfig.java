af4ea64937ff34eb2ccae2183aa7e761b665c6d5
lea.kirotar <lea.pullerits@gmail.com>
2025-03-18 09:48:19 +0200

    Added CORS and exception for room/search.

--- src/main/java/ee/rara/bruno/bruno/config/WebSecurityConfig.java
+++ src/main/java/ee/rara/bruno/bruno/config/WebSecurityConfig.java
@@ -1,14 +1,19 @@
 package ee.rara.bruno.bruno.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -18,29 +23,41 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home", "/login", "/h2-console/**", "/swagger-ui/**", "/v3/api-docs").permitAll()
+                        .requestMatchers("/", "/home", "/login", "/h2-console/**", "/swagger-ui/**", "/v3/api-docs", "/rooms/search").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
                         //.loginPage("/login")
                         .defaultSuccessUrl("/demo", true)
-                        //.successHandler(new SavedRequestAwareAuthenticationSuccessHandler())
                         .failureUrl("/login?error=true")
                         .permitAll()
                 )
-       /*         .exceptionHandling((exceptions) -> exceptions
-                        .authenticationEntryPoint((request, response, authException) -> {
-                            RequestCache requestCache = new HttpSessionRequestCache();
-                            requestCache.saveRequest(request, response);
-                            response.sendRedirect("/login");
-                        })
-                )*/
+                /*           .exceptionHandling((exceptions) -> exceptions
+                   .authenticationEntryPoint((request, response, authException) -> {
+                       RequestCache requestCache = new HttpSessionRequestCache();
+                       requestCache.saveRequest(request, response);
+                       response.sendRedirect("/login");
+                   })
+           )*/
                 .logout(LogoutConfigurer::permitAll);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
