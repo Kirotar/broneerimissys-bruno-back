387892a22536f41f012aa661ef9a805e6c3f4298
lea.kirotar <lea.pullerits@gmail.com>
2025-03-21 14:34:01 +0200

    Added swagger summaries.

--- src/main/java/ee/rara/bruno/bruno/controller/BookingController.java
+++ src/main/java/ee/rara/bruno/bruno/controller/BookingController.java
@@ -5,6 +5,7 @@ import ee.rara.bruno.bruno.dto.RoomSearch;
 import ee.rara.bruno.bruno.model.Booking;
 import ee.rara.bruno.bruno.service.BookingService;
 import ee.rara.bruno.bruno.service.PaymentService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,36 +23,43 @@ public class BookingController {
     }
 
     @PostMapping("/temp-booking")
+    @Operation(summary = "Teeb ajutiselt broneeringu.")
     public void addTemporaryBooking(@RequestBody BookingRequest booking) {
         bookingService.temporaryBooking(booking);
     }
 
     @PostMapping("/add-booking")
+    @Operation(summary = "Lisab broneeringu andmebaasi.")
     public void addBooking(@RequestBody BookingRequest booking) {
         bookingService.addBooking(booking);
     }
 
     @PostMapping("/add-repeat-booking")
+    @Operation(summary = "Lisab andmebaasi intervalli tagant korduva broneeringu.")
     public void addRepeatedBooking(@RequestBody BookingRequest booking) {
         bookingService.addRepeatedBooking(booking);
     }
 
     @DeleteMapping("/delete-booking")
+    @Operation(summary = "Kustutab broneeringu.")
     public void deleteBooking(@RequestBody BookingRequest booking) {
         bookingService.deleteBooking(booking);
     }
 
     @GetMapping("/get-bookings/{id}")
+    @Operation(summary = "Kasutaja broneeringud ID järgi.")
     public List<Booking> getUserBookingsByUserId(@PathVariable ("id") int id) {
         return bookingService.getUserBookingsByUserId(id);
     }
     @GetMapping("/get-room-availability")
+    @Operation(summary = "Kontrollib ruumi saadavust.")
     public boolean getRoomAvailability(@ModelAttribute BookingRequest query) {
         return bookingService.isRoomAvailable(query);
     }
 
     //admin
     @GetMapping("/all-bookings")
+    @Operation(summary = "Kõik broneeringud.")
     public List<Booking> getAllBookings() {
         return bookingService.getAllBookings();
     }
--- src/main/java/ee/rara/bruno/bruno/controller/RoomController.java
+++ src/main/java/ee/rara/bruno/bruno/controller/RoomController.java
@@ -21,32 +21,43 @@ public class RoomController {
     }
 
     @GetMapping("/all")
-    @Operation(summary = "Kõik ruumid")
+    @Operation(summary = "Kõik ruumid.")
     public List<Room> getAllRooms() {
         return roomService.getAllRooms();
     }
 
     @GetMapping("/search")
+    @Operation(summary = "Ruumiotsing")
     public List<Room> getSearchedRooms(@ModelAttribute RoomSearch search) {
         return roomService.findSearchedRooms(search);
     }
 
+    @GetMapping("/get-bookable-rooms")
+    @Operation(summary = "Kõik ruumid, mida on võimalik ise broneerida.")
+    public List<Room> getAllBookableRooms() {
+        return roomService.getAllBookableRooms();
+    }
+
     @GetMapping("/is-bookable/{id}")
+    @Operation(summary = "Kas saab broneerida jah/ei.")
     public boolean isBookable(@PathVariable int id) {
         return roomService.isRoomBookable(id);
     }
 
     @PostMapping("/add-room")
+    @Operation(summary = "Lisab ruumi.")
     public void createRoom(@RequestBody RoomChangeRequest request) {
         roomService.createRoom(request);
     }
 
     @DeleteMapping("/delete-room/{id}")
+    @Operation(summary = "Kustutab ruumi.")
     public void deleteRoom(@PathVariable("id") int id) {
         roomService.deleteRoom(id);
     }
 
     @PutMapping("/change-room/{id}")
+    @Operation(summary = "Muudab ruumi andmeid.")
     public void updateRoom(@PathVariable("id") int id, @RequestBody RoomChangeRequest updateRequest) {
         roomService.updateRoom(id, updateRequest);
     }
